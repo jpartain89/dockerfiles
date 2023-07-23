24efaa30f0e7d9961d1ef62ad14ef8f8d204c876
Justin P <jpartain89@gmail.com>
2023-07-23 18:14:37 -0500

    Update for 07-23-2023 18-14-37

--- install/install_torbrowser.sh
+++ install/install_torbrowser.sh
@@ -16,15 +16,16 @@ wget --quiet "${FULL_TOR_URL}" -O /tmp/torbrowser.tar.xz
 tar -xJf /tmp/torbrowser.tar.xz -C $TOR_HOME
 rm /tmp/torbrowser.tar.xz
 
-cp $TOR_HOME/start-tor-browser.desktop $TOR_HOME/start-tor-browser.desktop.bak
-cp $TOR_HOME/Browser/browser/chrome/icons/default/default128.png /usr/share/icons/tor.png
+
+cp $TOR_HOME/tor-browser/start-tor-browser.desktop $TOR_HOME/tor-browser/start-tor-browser.desktop.bak
+cp $TOR_HOME/tor-browser/Browser/browser/chrome/icons/default/default128.png /usr/share/icons/tor.png
 chown 1000:0 /usr/share/icons/tor.png
-sed -i 's/^Name=.*/Name=Tor Browser/g' $TOR_HOME/start-tor-browser.desktop
-sed -i 's/Icon=.*/Icon=\/usr\/share\/icons\/tor.png/g' $TOR_HOME/start-tor-browser.desktop
-sed -i 's/Exec=.*/Exec=sh -c \x27"$HOME\/tor-browser\/tor-browser\/Browser\/start-tor-browser" --detach || ([ !  -x "$HOME\/tor-browser\/tor-browser\/Browser\/start-tor-browser" ] \&\& "$(dirname "$*")"\/Browser\/start-tor-browser --detach)\x27 dummy %k/g'  $TOR_HOME/start-tor-browser.desktop
+sed -i 's/^Name=.*/Name=Tor Browser/g' $TOR_HOME/tor-browser/start-tor-browser.desktop
+sed -i 's/Icon=.*/Icon=\/usr\/share\/icons\/tor.png/g' $TOR_HOME/tor-browser/start-tor-browser.desktop
+sed -i 's/Exec=.*/Exec=sh -c \x27"$HOME\/tor-browser\/tor-browser\/Browser\/start-tor-browser" --detach || ([ !  -x "$HOME\/tor-browser\/tor-browser\/Browser\/start-tor-browser" ] \&\& "$(dirname "$*")"\/Browser\/start-tor-browser --detach)\x27 dummy %k/g'  $TOR_HOME/tor-browser/start-tor-browser.desktop
 
 
-cat >> $TOR_HOME/Browser/TorBrowser/Data/Browser/profile.default/prefs.js <<EOL
+cat >> $TOR_HOME/tor-browser/Browser/TorBrowser/Data/Browser/profile.default/prefs.js <<EOL
 user_pref("app.normandy.enabled", false);
 user_pref("app.update.download.promptMaxAttempts", 0);
 user_pref("app.update.elevation.promptMaxAttempts", 0);
@@ -73,9 +74,10 @@ EOL
 
 # Maintain backwards compatability with old image definitions that expect tor to be launched from /tmp
 mkdir -p /tmp/tor-browser/Browser/
-ln -s $TOR_HOME/start-tor-browser.desktop /tmp/tor-browser/Browser/start-tor-browser.desktop\
+ln -s $TOR_HOME/tor-browser/start-tor-browser.desktop /tmp/tor-browser/Browser/start-tor-browser.desktop\
 
 chown -R 1000:0 $TOR_HOME/
 
-cp $TOR_HOME/start-tor-browser.desktop $HOME/Desktop/
+
+cp $TOR_HOME/tor-browser/start-tor-browser.desktop $HOME/Desktop/
 chown 1000:0  $HOME/Desktop/start-tor-browser.desktop
